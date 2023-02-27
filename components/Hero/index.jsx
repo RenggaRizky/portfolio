535859636232106adffc8ab86c30921238876c67
RenggaRizky <renggarizkys@gmail.com>
2023-02-27 16:44:19 +0700

    add new project

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -3,44 +3,31 @@ import PrimaryBtn from "../Button/Primary";
 
 export default function Hero() {
     return (
-        <section className="mt-8 mb-16 sm:mx-auto sm:max-w-2xl sm:px-4 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-            <div className="container px-4 sm:bg-gradient-to-r from-darkblue to-secondary sm:rounded-xl sm:flex sm:justify-between sm:gap-x-8 sm:shadow-lg lg:px-16 xl:gap-x-10">
-                <div className="flex justify-center sm:hidden">
-                    <img src="img/hero-m.svg" alt="hero" className="w-full" />
+        <section className='mt-8 mb-16 sm:mx-auto sm:max-w-2xl sm:px-4 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl'>
+            <div className='container px-4 sm:bg-gradient-to-r from-darkblue to-secondary sm:rounded-xl sm:flex sm:justify-between sm:gap-x-8 sm:shadow-lg lg:px-16 xl:gap-x-10'>
+                <div className='flex justify-center sm:hidden'>
+                    <img src='img/hero-m.svg' alt='hero' className='w-full' />
                 </div>
 
-                <div className="px-2 sm:basis-1/2 sm:self-center sm:py-8 lg:py-20">
-                    <p className="uppercase text-primary font-bold text-sm mt-16 sm:mb-3 sm:mt-0 lg:text-base lg:mb-4">
-                        Halo, nama saya
-                    </p>
+                <div className='px-2 sm:basis-1/2 sm:self-center sm:py-8 lg:py-20'>
+                    <p className='uppercase text-primary font-bold text-sm mt-16 sm:mb-3 sm:mt-0 lg:text-base lg:mb-4'>Halo, nama saya</p>
 
-                    <h1 className="uppercase text-2xl font-bold sm:text-white sm:text-3xl lg:text-5xl lg:mb-4 xl:text-6xl xl:mb-8">
-                        Rengga Rizky Septian
-                    </h1>
+                    <h1 className='uppercase text-2xl font-bold sm:text-white sm:text-3xl lg:text-5xl lg:mb-4 xl:text-6xl xl:mb-8'>Rengga Rizky Septian</h1>
 
-                    <p className="font-normal text-xs text-slate-400 text-justify mt-2 sm:text-lightpurple sm:text-left sm:text-[0.625rem] xl:text-sm">
-                        Lulusan Universitas Pancasila dari jurusan Teknik
-                        Informatika yang memiliki ketertarikan dan minat yang
-                        tinggi untuk berkarir di bidang pengembangan web,
-                        khususnya di posisi Front End Developer.
-                    </p>
+                    <p className='font-normal text-xs text-slate-400 text-justify mt-2 sm:text-lightpurple sm:text-left sm:text-[0.625rem] xl:text-sm'>Lulusan Universitas Pancasila dari jurusan Teknik Informatika yang memiliki ketertarikan dan minat yang tinggi untuk berkarir di bidang pengembangan apilkasi, khususnya di posisi Software Engineer, Application Developer, dan Front End Developer.</p>
 
-                    <a href="#experiences">
-                        <PrimaryBtn className="mt-20 w-full flex justify-center items-center sm:w-auto sm:mt-10 lg:mt-12 xl:mt-20">
+                    <a href='#experiences'>
+                        <PrimaryBtn className='mt-20 w-full flex justify-center items-center sm:w-auto sm:mt-10 lg:mt-12 xl:mt-20'>
                             Lihat selengkapnya
-                            <span className="rounded-full bg-darkblue px-2 py-1 ml-6 sm:ml-3 lg:ml-6">
-                                <i className="fa-solid fa-arrow-down text-primary"></i>
+                            <span className='rounded-full bg-darkblue px-2 py-1 ml-6 sm:ml-3 lg:ml-6'>
+                                <i className='fa-solid fa-arrow-down text-primary'></i>
                             </span>
                         </PrimaryBtn>
                     </a>
                 </div>
 
-                <div className="sm:w-full sm:basis-1/2 sm:self-end">
-                    <img
-                        src="img/hero.png"
-                        alt="hero"
-                        className="hidden sm:inline-block sm:w-full "
-                    />
+                <div className='sm:w-full sm:basis-1/2 sm:self-end'>
+                    <img src='img/hero.png' alt='hero' className='hidden sm:inline-block sm:w-full ' />
                 </div>
             </div>
         </section>
--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
         company: "Toko Aly Jaya Ciomas",
         img: [
             {
-                src: "img/project-1-1.svg",
+                src: "img/project-1-1.png",
                 alt: "Dashboard Sistem Informasi Manajemen Inventory",
             },
             {
@@ -27,85 +27,74 @@ export default function Projects() {
                 alt: "Sistem Informasi Manajemen Inventory Halaman Rak",
             },
         ],
-        appDesc:
-            "Sistem ini dibuat untuk membantu Toko Aly Jaya Ciomas dalam melakukan manajemen inventory produk yang ada di toko maupun di gudang. Sistem ini dapat melakukan pendataan produk, pendataan merek, pendataan jenis barang, pendataan barang masuk, pendataan barang keluar, pendataan pereturan barang, pendataan supplier, pendataan stok barang, mencetak laporan, pendataan kapasitas dan lokasi rak, serta notifikasi status stok barang",
-        toolsDesc:
-            "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
-        tools: [
-            "ReactJS",
-            "NodeJS",
-            "ExpressJS",
-            "Moongose",
-            "MongoDB Atlas",
-            "Bootstrap",
-            "axios",
-            "html2canvas",
-            "jsPDF",
-            "moment",
-        ],
+        appDesc: "Sistem ini dibuat untuk membantu Toko Aly Jaya Ciomas dalam melakukan manajemen inventory produk yang ada di toko maupun di gudang. Sistem ini dapat melakukan pendataan produk, pendataan merek, pendataan jenis barang, pendataan barang masuk, pendataan barang keluar, pendataan pereturan barang, pendataan supplier, pendataan stok barang, mencetak laporan, pendataan kapasitas dan lokasi rak, serta notifikasi status stok barang",
+        toolsDesc: "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
+        tools: ["ReactJS", "NodeJS", "ExpressJS", "Moongose", "MongoDB Atlas", "Bootstrap", "axios", "html2canvas", "jsPDF", "moment"],
         code: "https://github.com/RenggaRizky/inventory-management",
     };
 
     const dataProject2 = {
-        title: "Web Pendataan Kasus COVID 19",
-        company: "",
-        img: [
-            {
-                src: "img/project-2-1.png",
-                alt: "Hero Web Pendataan Kasus COVID 19",
-            },
-            {
-                src: "img/project-2-2.png",
-                alt: "Informasi Kasus COVID 19",
-            },
-            {
-                src: "img/project-2-3.png",
-                alt: "Hasil Pencarian Kasus COVID 19",
-            },
-        ],
-        appDesc:
-            "Web ini mampu mengetahui data terkini mengenai COVID 19 di berbagai negara seperti jumlah pendududuk yang terinfeksi, jumlah penduduk yang meninggal, jumlah penduduk yang sembuh, dan jumlah penduduk yang masih menderita virus COVID 19",
-        toolsDesc:
-            "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
-        tools: ["HTML", "SCSS", "JavaScript", "Bootstrap", "Axios"],
-        code: "https://github.com/RenggaRizky/web-pendataan-kasus-covid-19",
-    };
-
-    const dataProject3 = {
         title: "Moviearo  Penyedia Informasi Film & Serial TV",
         company: "The Movie Database API",
         img: [
             {
-                src: "img/project-3-1.png",
+                src: "img/project-2-1.png",
                 alt: "Halaman Utama Moviearo",
             },
             {
-                src: "img/project-3-2.png",
+                src: "img/project-2-2.png",
                 alt: "Halaman List Film dan Serial TV",
             },
             {
-                src: "img/project-3-3.png",
+                src: "img/project-2-3.png",
                 alt: "Halaman Film dan Serial TV",
             },
             {
-                src: "img/project-3-4.png",
+                src: "img/project-2-4.png",
                 alt: "Section video",
             },
         ],
-        appDesc:
-            "Web yang dibuat untuk menampilkan informasi mengenai film dan serial tv di seluruh dunia dimana data yang diambil berasal dari The  Movie Database API",
-        toolsDesc:
-            "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
-        tools: ["ReactJS", "Tailwind", "Florbite Tailwind"],
+        appDesc: "Web yang dibuat untuk menampilkan informasi mengenai film dan serial tv di seluruh dunia dimana data yang diambil berasal dari The  Movie Database API",
+        toolsDesc: "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
+        tools: ["ReactJS", "Tailwind", "Flowbite Tailwind"],
         code: "https://moviearo.netlify.app/",
+    };
+
+    const dataProject3 = {
+        title: "Landing Page Echo Manajemen Portofolio Crypto",
+        company: "Landing Page",
+        img: [
+            {
+                src: "img/project-3-1.png",
+                alt: "Halaman Utama Echo",
+            },
+            {
+                src: "img/project-3-2.png",
+                alt: "Modal di Halaman Utama Echo",
+            },
+            {
+                src: "img/project-3-3.png",
+                alt: "Section Fitur Echo",
+            },
+            {
+                src: "img/project-3-4.png",
+                alt: "Section Keuntungan Echo",
+            },
+            {
+                src: "img/project-3-5.png",
+                alt: "Section Blog Echo",
+            },
+        ],
+        appDesc: "Contoh web landing page dari perusahaan manajemen portofolio crypto",
+        toolsDesc: "Berikut merupakan beberapa teknologi yang dipakai untuk membuat sistem: ",
+        tools: ["HTML", "CSS", "JavaScript"],
+        code: "https://renggarizky.github.io/crypto-portfolio-management-landingpage/",
     };
 
     const project1 = () => {
         confirmAlert({
             customUI: ({ onClose }) => {
-                return (
-                    <ModalCard onClose={onClose} dataProject={dataProject1} />
-                );
+                return <ModalCard onClose={onClose} dataProject={dataProject1} sourceType='code' />;
             },
         });
     };
@@ -113,9 +102,7 @@ export default function Projects() {
     const project2 = () => {
         confirmAlert({
             customUI: ({ onClose }) => {
-                return (
-                    <ModalCard onClose={onClose} dataProject={dataProject2} />
-                );
+                return <ModalCard onClose={onClose} dataProject={dataProject2} sourceType='app' />;
             },
         });
     };
@@ -123,86 +110,48 @@ export default function Projects() {
     const project3 = () => {
         confirmAlert({
             customUI: ({ onClose }) => {
-                return (
-                    <ModalCard onClose={onClose} dataProject={dataProject3} />
-                );
+                return <ModalCard onClose={onClose} dataProject={dataProject3} sourceType='app' />;
             },
         });
     };
 
     return (
-        <section
-            className="container px-4 mb-16 mx-auto lg:max-w-5xl xl:max-w-7xl pt-4 lg:pt-8"
-            id="projects"
-        >
+        <section className='container px-4 mb-16 mx-auto lg:max-w-5xl xl:max-w-7xl pt-4 lg:pt-8' id='projects'>
             <div>
-                <p className="uppercase text-primary font-bold text-sm lg:text-base">
-                    Projects
-                </p>
+                <p className='uppercase text-primary font-bold text-sm lg:text-base'>Projects</p>
 
-                <h2 className="text-darkblue font-bold text-xl mb-8 sm:text-2xl lg:text-3xl">
-                    Pernah buat apa aja nih??..
-                </h2>
-
-                <div className="flex flex-col justify-center items-center gap-y-4 sm:flex-row sm:gap-y-0 sm:gap-x-4 sm:items-stretch sm:justify-evenly sm:px-5 lg:justify-center xl:justify-start">
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
+                <h2 className='text-darkblue font-bold text-xl mb-8 sm:text-2xl lg:text-3xl'>Pernah buat apa aja nih??..</h2>
+                <div className='grid grid-cols-1 justify-center items-center gap-y-4 px-2 sm:grid-cols-2 sm:gap-x-4 sm:justify-between sm:items-stretch sm:px-5 lg:grid-cols-3'>
+                    <div className='sm:max-w-sm bg-white border border-gray-200 rounded-lg shadow-md w-full'>
                         <div>
-                            <img
-                                className="rounded-t-lg sm:w-full"
-                                src="img/project-1-1.svg"
-                                alt=""
-                            />
+                            <img className='rounded-t-lg sm:w-full' src='img/project-1-1.png' alt='' />
                         </div>
-                        <div className="p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs">
-                            <h5 className="text-lg font-bold tracking-tight text-darkblue mb-4 leading-5 sm:text-base sm:mb-2 md:text-lg">
-                                Sistem Informasi Manajemen Inventory
-                            </h5>
-                            <p className="mb-10 font-normal text-xs  uppercase text-slate-400 sm:text-[0.625rem] xl:text-xs">
-                                toko aly jaya ciomas
-                            </p>
-                            <ModalBtn onClick={project1}>
-                                Lihat Selengkapnya
-                            </ModalBtn>
+                        <div className='p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs'>
+                            <h5 className='text-lg font-bold tracking-tight text-darkblue mb-4 leading-5 sm:text-base sm:mb-2 md:text-lg'>Sistem Informasi Manajemen Inventory Toko Aly Jaya Ciomas</h5>
+                            <p className='mb-10 font-normal text-xs  uppercase text-slate-400 sm:text-[0.625rem] xl:text-xs'>React, NodeJS, ExpressJS, MongoDB</p>
+                            <ModalBtn onClick={project1}>Lihat Selengkapnya</ModalBtn>
                         </div>
                     </div>
 
-                    {/* <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
+                    <div className='sm:max-w-sm bg-white border border-gray-200 rounded-lg shadow-md w-full'>
                         <div>
-                            <img
-                                className="rounded-t-lg sm:bg-cover"
-                                src="img/project-2.svg"
-                                alt=""
-                            />
+                            <img className='rounded-t-lg sm:w-full' src='img/project-2-1.png' alt='' />
                         </div>
-                        <div className="p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs">
-                            <h5 className="mb-2 text-lg font-bold tracking-tight text-darkblue leading-5 sm:text-base sm:mb-2 md:mb-10 md:text-lg">
-                                Web Pendataan Kasus COVID 19
-                            </h5>
-                            <p className="mb-10 font-normal text-xs  uppercase text-slate-400 sm:mb-[4.5rem] sm:flex sm:justify-between sm:flex-col sm:text-xs xl:text-xs"></p>
-                            <ModalBtn onClick={project2}>
-                                Lihat Selengkapnya
-                            </ModalBtn>
+                        <div className='p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs'>
+                            <h5 className='text-lg font-bold tracking-tight text-darkblue mb-4 leading-5 sm:text-base sm:mb-2 md:text-lg'>Moviearo Penyedia Informasi Film & Serial TV (The Movie Database API)</h5>
+                            <p className='mb-10 font-normal text-xs  uppercase text-slate-400 sm:text-[0.625rem] xl:text-xs'>React, Tailwind</p>
+                            <ModalBtn onClick={project2}>Lihat Selengkapnya</ModalBtn>
                         </div>
-                    </div> */}
+                    </div>
 
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
+                    <div className='sm:max-w-sm bg-white border border-gray-200 rounded-lg shadow-md w-full'>
                         <div>
-                            <img
-                                className="rounded-t-lg sm:w-full"
-                                src="img/project-3-1.png"
-                                alt=""
-                            />
+                            <img className='rounded-t-lg sm:w-full' src='img/project-3-1.png' alt='' />
                         </div>
-                        <div className="p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs">
-                            <h5 className="text-lg font-bold tracking-tight text-darkblue mb-4 leading-5 sm:text-base sm:mb-2 md:text-lg">
-                                Moviearo Penyedia Informasi Film & Serial TV
-                            </h5>
-                            <p className="mb-10 font-normal text-xs  uppercase text-slate-400 sm:text-[0.625rem] xl:text-xs">
-                                The Movie Database API
-                            </p>
-                            <ModalBtn onClick={project3}>
-                                Lihat Selengkapnya
-                            </ModalBtn>
+                        <div className='p-5 sm:flex sm:justify-between sm:flex-col sm:text-xs'>
+                            <h5 className='text-lg font-bold tracking-tight text-darkblue mb-4 leading-5 sm:text-base sm:mb-2 md:text-lg'>Landing Page Echo Manajemen Portofolio Crypto</h5>
+                            <p className='mb-10 font-normal text-xs  uppercase text-slate-400 sm:text-[0.625rem] xl:text-xs'>HTML, CSS, JavaScript</p>
+                            <ModalBtn onClick={project3}>Lihat Selengkapnya</ModalBtn>
                         </div>
                     </div>
                 </div>
